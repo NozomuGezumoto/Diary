eca52bd84b949ca98a1e455414747ca79ce3073a
枳本望 <nozomusp29@gmail.com>
2019-11-15 11:39:56 +0800

    edit機能追加

--- resources/views/diaries/index.blade.php
+++ resources/views/diaries/index.blade.php
@@ -1,22 +1,32 @@
+<!-- layout.blade.phpを読み込む -->
 @extends('layout')
 
 @section('title', '一覧')
 
 @section('content')
+
   <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">新規投稿</a>
-   @foreach ($diaries as $diary)
-   <div class="m-4 p-4 border border-primary">
-   <p>{{$diary->title}}</p>
-   <p>{{$diary->body}}</p>
-   <p>{{$diary->created_at}}</p>
-<!-- //delete -->
-<form action="{{ route('diary.destroy',['id' => $diary->id]) }}" method="POST" class="d-inline">
-@csrf
-@method('delete')
-<button class="btn btn-danger">削除</button>
-</form>
+  @foreach ($diaries as $diary)
 
-   </div>
-   @endforeach
 
-   @endsection
+  <div class="m-4 p-4 border border-primary">
+    <p>{{$diary->title}}</p>
+    <p>{{$diary->body}}</p>
+    <p>{{$diary->created_at}}</p>
+
+
+
+      <!-- formメソッドではPOST or GET 送信しかできないので＠で記載 -->
+      <a href="{{ route('diary.edit', ['id' => $diary->id]) }}"class="btn btn-success">編集</a>
+
+   <!-- //削除するためのform (webからの)-->
+    <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="POST" class="d-inline">
+    @csrf
+    <!-- formメソッドではPOST or GET 送信しかできないので＠で記載 -->
+    @method('delete')
+    <button class="btn btn-danger">削除</button>
+    </form>
+  </div>
+  @endforeach
+
+  @endsection
